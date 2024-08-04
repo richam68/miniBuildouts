87fe1f7ebce49c6447e95fd3813cf6327b69bafb
richam68 <richamhshwr2@gmail.com>
2024-08-04 10:33:13 +0530

    first commit

---
+++ src/components/country.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+const Country = () => {
+  const [countryData, setCountryData] = useState([]);
+  const [selectCountry, setSelectCountry] = useState("");
+
+  const [stateData, setStateData] = useState([]);
+  const [selectState, setSelecteState] = useState('');
+
+  const [cityData, setCityData] = useState([]);
+  const [selectCity, setSelectCity] = useState('');
+
+  useEffect(() => {
+    countryApi();
+  }, []);
+
+  useEffect(() => {
+    if(selectCountry){
+        stateApi()
+    }
+  }, [selectCountry]);
+
+  useEffect(() => {
+    if(selectCountry && selectState){
+        cityApi()
+    }
+  }, [selectCountry, selectState])
+
+  const countryApi = async () => {
+    try {
+      let response = await fetch(
+        "https://crio-location-selector.onrender.com/countries"
+      );
+      let responseData = await response.json();
+      setCountryData(responseData);
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
+  const stateApi = async () => {
+    try {
+      let response = await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectCountry}/states`
+      );
+      let responseData = await response.json();
+      setStateData(responseData)
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
+  const cityApi = async() => {
+    try{
+    let response = await fetch(`https://crio-location-selector.onrender.com/country=${selectCountry}/state=${selectState}/cities`);
+    let responseData = await response.json();
+    console.log("responseData", responseData);
+    setCityData(responseData)
+    }catch(err){
+        console.log("err", err);
+    }
+  }
+
+  const handleCountryChange = (e) => {
+    setSelectCountry(e.target.value);
+    setSelecteState(""); // Reset state and city when country changes
+    setCityData([]);
+    setSelectCity("");
+  };
+
+  const handleStateChange = (e) => {
+    setSelecteState(e.target.value) 
+    setCityData([]); // Reset city when state changes
+    setSelectCity("");
+  }
+
+  const handleSelectCity = (e) => {
+    setSelectCity(e.target.value)
+  }
+  return (
+    <div>
+      <h1>Select Location</h1>
+      <div className="select-wrapper">
+        <select onChange={handleCountryChange} value={selectCountry}>
+          <option>Select Country</option>
+          {countryData.map((item, i) => {
+            return (
+              <option key={i + 1} value={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+
+        <select onChange={handleStateChange} value={selectState} disabled={!selectCountry}>
+          <option>Select State</option>
+          {stateData.map((item, i) => {
+            return <option key={i+1} value={item}>{item}</option>;
+          })}
+        </select>
+
+        <select onChange={handleSelectCity} value={selectCity} disabled={!selectState}>
+          <option >Select City</option>
+          {cityData.map((item, i) => {
+            return (
+                <option key={i+1} value={item}>{item}</option>
+            )
+          })}
+        </select>
+      </div>
+      {selectCountry && selectState && selectCity && (
+      <div>
+        <h2>You selected
+        <span className="highlight">{selectCountry}</span>,{" "}
+          <span className="fade">
+            {selectState}, {selectCity}
+          </span>
+            </h2>
+      </div>
+)}
+    </div>
+  );
+};
+
+export default Country;
